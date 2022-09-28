9c482fef351ab3fc2b3b2a0a468451917a8cbb75
PICCI-RIDOY <107355397+RIDOY-404-CYBER@users.noreply.github.com>
2022-09-28 10:14:50 +0600

    Update RMX-PRO.py

--- RMX-PRO.py
+++ RMX-PRO.py
@@ -2,9 +2,12 @@ import os,platform
 
 os.system('clear')
 
-print('\033[1;32m[•] This Tools is Currently off Wait Sometime...')
+print('\033[1;32m[•] Random Tools is Currently off Wait Sometime...')
 
+print('\033[1;32m[•] Public Cloning Run💚
+print('\033[1;32m[•] python Wowo.py
 
-
+print('\033[1;32m[•] File Cloning Run💚
+print('\033[1;32m[•] python Wowo2.py
     
 
