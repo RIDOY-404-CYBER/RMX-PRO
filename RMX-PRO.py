b6365b9f918099a473ff9a9bcfab11f4fe183612
PICCI-RIDOY <107355397+RIDOY-404-CYBER@users.noreply.github.com>
2022-10-20 21:02:07 +0600

    Update RMX-PRO.py

--- RMX-PRO.py
+++ RMX-PRO.py
@@ -1,18 +1,11 @@
-import os, platform, time
-print('\n\x1b[1;37m Checking Update...');time.sleep(0.5)
-os.system('git pull')
-try:
-    import wowx
-except IOError:
-    os.system('pip install gtts')
-def Update():
-    exit('\033[1;31m(×) Commands On Update Please Wait For Update ❤ ')
-def rmx():
-        bit = platform.architecture()[0]
-        if bit == '64bit':
-            print("\x1b[1;92m Congratulations ! Your Device Support this Tools")
+import os, sys
 
-            import wowx
-        else:
-            exit('\033[1;31m[×] Device Not Support 32bit')
-rmx()
+try:
+print('\033[1;32m[•] Congrats! Your Device Support This Tools \033[1;37m')
+
+    os.system('xdg-open https://facebook.com/groups/770617227293870/')
+    __import__("wowx").rmx()
+
+except Exception as e:
+
+    exit(str(e))
