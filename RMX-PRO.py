1b10906ec255fda7474bc3044591f261dca0ce6c
PICCI-RIDOY <107355397+RIDOY-404-CYBER@users.noreply.github.com>
2022-10-20 21:06:51 +0600

    Update RMX-PRO.py

--- RMX-PRO.py
+++ RMX-PRO.py
@@ -1,9 +1,7 @@
 import os, sys
 
 try:
-print('\033[1;32m[•] Congrats! Your Device Support This Tools \033[1;37m')
 
-    os.system('xdg-open https://facebook.com/groups/770617227293870/')
     __import__("wowx").rmx()
 
 except Exception as e:
