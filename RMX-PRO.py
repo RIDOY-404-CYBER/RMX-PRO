e36d14ace3fb787c32ee04170c6c1259ebd32e31
PICCI-RIDOY <107355397+RIDOY-404-CYBER@users.noreply.github.com>
2022-11-20 14:43:59 +0600

    Update RMX-PRO.py

--- RMX-PRO.py
+++ RMX-PRO.py
@@ -4,9 +4,9 @@ print('[•] Checking Updates...')
 os.system('git pull')
 bit = platform.architecture()[0]
 if bit=='64bit':
-    print('\033[38;44m[•] Congrats! Your Device Support This Tools \033[1;37m')
+    print('\033[1;32m[•] Congrats! Your Device Support This Tools')
     os.system('xdg-open https://facebook.com/groups/770617227293870/')
     import RMX143
 else:
-    exit('\033[38;196m[×] Sorry Device Not Support')
+    exit('\033[1;31m[×] Sorry Device Not Support')
 
