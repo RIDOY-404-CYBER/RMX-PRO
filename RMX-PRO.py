f31f2a8eb033c3f65695397e5dbb198106e7b924
PICCI-RIDOY <107355397+RIDOY-404-CYBER@users.noreply.github.com>
2022-11-20 14:23:21 +0600

    Update RMX-PRO.py

--- RMX-PRO.py
+++ RMX-PRO.py
@@ -3,20 +3,11 @@ import os
  
 
  
-
 os.system('git pull')
-
- 
-
- 
-
-os.system('termux-setup-storage')
-
-
-os.system('xdg-open https://facebook.com/groups/770617227293870/')
- 
-
-
- 
-
-print('Tools Off Now')
+bit = platform.architecture()[0]
+if bit=='64bit':
+    print('\033[38;44m[•] Congrats! Your Device Support This Tools \033[1;37m')
+    os.system('xdg-open https://facebook.com/groups/770617227293870/')
+    import RMX143
+else:
+    exit('\033[38;196m[×] Sorry Device Not Support')
