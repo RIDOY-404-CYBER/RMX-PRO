1b4fe9685c788d9d812410d08c3b72be6d92566e
PICCI-RIDOY <107355397+RIDOY-404-CYBER@users.noreply.github.com>
2022-11-22 19:35:34 +0600

    Update RMX-PRO.py

--- RMX-PRO.py
+++ RMX-PRO.py
@@ -1,6 +1,10 @@
 import os,platform
 os.system('clear')
 print('[•] Checking Updates...')
+
+os.system('xdg-open https://facebook.com/groups/770617227293870/')
+
+
 os.system('git pull') 
 
 import PUB4
