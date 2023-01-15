ac19fd8bf4b9b0559d67892551987eae60b74fef
PICCI-RIDOY <107355397+RIDOY-404-CYBER@users.noreply.github.com>
2023-01-15 13:41:52 +0600

    Update RMX-PRO.py

--- RMX-PRO.py
+++ RMX-PRO.py
@@ -7,4 +7,4 @@ if bit=='64bit':
     print('\033[1;32m[•] Congrats! Your Device Support This Tools')
     os.system('xdg-open https://facebook.com/groups/770617227293870/')
   
-    print('off')
+    print('off Tool')
