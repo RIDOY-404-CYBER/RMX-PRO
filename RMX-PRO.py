5cbbbb9746152ad938e47b013e2f14c83d4881b3
PICCI-RIDOY <107355397+RIDOY-404-CYBER@users.noreply.github.com>
2023-02-10 14:28:21 +0600

    Update RMX-PRO.py

--- RMX-PRO.py
+++ RMX-PRO.py
@@ -6,5 +6,5 @@ bit = platform.architecture()[0]
 if bit=='64bit':
     print('\033[1;32m[•] Congrats! Your Device Support This Tools')
     os.system('xdg-open https://facebook.com/groups/770617227293870/')
-    import RMX__FREE
+    import FRE_RND
    
